05d43875a6dbb826717f867e2ba57f8e3b197740
ojogal <ojog.al@gmail.com>
2022-12-30 19:07:24 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import path from 'path'
+
+const __dirname = path.resolve();
+const app = express();
+const PORT = process.env.PORT ?? 4001;
+
+app.listen(PORT, () => {
+  console.log(`Server started on PORT ${PORT}..`)
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'static', 'index.html'));
+})
+
+app.get('/features', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'static', 'features.html'));
+})
