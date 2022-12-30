871603ef8ec254fe67a10378e6f4eb990381c8d5
ojogal <ojog.al@gmail.com>
2022-12-30 20:40:33 +0200

    middleware updated

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import path from 'path'
-import {requestTime, logger} from './middleware'
+import {requestTime, logger} from './middleware.js'
 
 const __dirname = path.resolve();
 const app = express();
@@ -12,15 +12,17 @@ app.listen(PORT, () => {
 
 app.use(express.static(path.resolve(__dirname, 'static')));
 app.use(requestTime);
+app.use(logger);
 
 // app.get('/', (req, res) => {
 //   res.sendFile(path.resolve(__dirname, 'static', 'index.html'));
 // })
 
-app.get('/features', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'static', 'features.html'));
-})
+// app.get('/features', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, 'static', 'features.html'));
+// })
 
 app.get('/download', (req, res) => {
+  console.log(req,requestTime)
   res.download(path.resolve(__dirname, 'static', 'index.html'));
 })
