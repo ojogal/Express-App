29fa8e7001705e75f7f07aa823950f65da8d3e73
ojogal <ojog.al@gmail.com>
2022-12-31 21:44:26 +0200

    routes users added

--- index.js
+++ index.js
@@ -1,30 +1,35 @@
-import express from 'express'
-import path from 'path'
-import {requestTime, logger} from './middleware.js'
-import serverRoutes from './routes/servers.js'
+import express from 'express';
+import bodyParser from 'body-parser';
+import userRoutes from './routes/users.js';
+// import path from 'path'
+// import {requestTime, logger} from './middleware.js'
+// import serverRoutes from './routes/servers.js'
 
-const __dirname = path.resolve();
+// const __dirname = path.resolve();
 const app = express();
 const PORT = process.env.PORT ?? 4001;
 
+app.use(bodyParser.json());
+app.use('/users', userRoutes);
+
 app.listen(PORT, () => {
-  console.log(`Server started on PORT ${PORT}..`)
+  console.log(`Server started on PORT http://localhost:${PORT}..`)
 });
 
 // app.use(express.static(path.resolve(__dirname, 'static')));
-app.use(requestTime);
-app.use(logger);
-app.set('view engine', 'ejs');
-app.set('views', path.resolve(__dirname, 'ejs'));
-app.use(serverRoutes);
+// app.use(requestTime);
+// app.use(logger);
+// app.set('view engine', 'ejs');
+// app.set('views', path.resolve(__dirname, 'ejs'));
+// app.use(serverRoutes);
 
-app.get('/', (req, res) => {
-  res.render('index', {title: 'Persha storinka', active: 'main'});
-});
+// app.get('/', (req, res) => {
+//   res.render('index', {title: 'Persha storinka', active: 'main'});
+// });
 
-app.get('/features', (req, res) => {
-  res.render('features', {title: 'Druga storinka', active: 'features'});
-});
+// app.get('/features', (req, res) => {
+//   res.render('features', {title: 'Druga storinka', active: 'features'});
+// });
 
 // app.get('/', (req, res) => {
 //   res.sendFile(path.resolve(__dirname, 'static', 'index.html'));
